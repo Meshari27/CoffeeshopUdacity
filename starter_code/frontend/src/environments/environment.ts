d83001045bedcc45bccbed389a75309ac3c137c6
Meshari Althubyani <mesharialthubyani@Mesharis-MacBook-Air-2.local>
2021-02-09 21:35:06 +0300

    done

--- starter_code/frontend/src/environments/environment.ts
+++ starter_code/frontend/src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: 'meshari-coffee.us.auth0.com', // the auth0 domain prefix
+    url: 'meshari-coffee.us', // the auth0 domain prefix
     audience: 'Coffee', // the audience set for the auth0 app
     clientId: 'Fh4bR5QVigUOIok2pKvj8t7tDKKIDOWd', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
